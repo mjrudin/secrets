51ec126a98f2b4ea1b8d8b57d12d0a75840e182d
Tommy Duek <taduek@gmail.com>
2013-07-25 18:41:53 -0400

    First commit in tommy branch. Added tag rendering in user show on both initial request and subsequent ajax requests

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -4,7 +4,7 @@ class Secret < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
   has_many :secret_taggings
-  has_many :tags, through: :secret_taggings
+  has_many :tags, through: :secret_taggings, dependent: :destroy
 
   validates :title, :author, :recipient, :presence => true
 end
